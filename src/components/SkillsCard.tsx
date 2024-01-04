b8a45c194657ee64f12b5b18af58b2fc8f5fd400
Pthalo000f89 <95905685+Pthalo000f89@users.noreply.github.com>
2024-01-03 22:22:07 -0800

    test 3

--- src/components/SkillsCard.tsx
+++ src/components/SkillsCard.tsx
@@ -1,68 +1,79 @@
 // import  codingIcons  from "../assets/coding icons/CodingIcons"
+import htmlIcon from '/src/assets/coding icons/html5-original.svg'
+import cssIcon from '/src/assets/coding icons/css3-original.svg'
+import javascriptIcon from '/src/assets/coding icons/javascript-original.svg'
+import typescriptIcon from '/src/assets/coding icons/typescript-original.svg'
+import npmIcon from '/src/assets/coding icons/icons8-npm-480.png'
+import figmaIcon from '/src/assets/coding icons/figma-original.svg'
+import githubIcon from '/src/assets/coding icons/github-original.svg'
+import reactIcon from '/src/assets/coding icons/react-original.svg'
+import sassIcon from '/src/assets/coding icons/sass-original.svg'
+import vscodeIcon from '/src/assets/coding icons/vscode-original.svg'
+
 
 const codingIcons = {
     html:{
-        icon:'./assets/coding icons/html5-original.svg',
-        iconText:'./assets/coding icons/html5-original-wordmark.svg',
-        simple: './assets/coding icons/html5-plain.svg',
-        simpleText:'./assets/coding icons/html5-plain-wordmark.svg',
+        icon:htmlIcon,
+        iconText:'/src/assets/coding icons/html5-original-wordmark.svg',
+        simple: '/src/assets/coding icons/html5-plain.svg',
+        simpleText:'/src/assets/coding icons/html5-plain-wordmark.svg',
     },
     css:{
-        icon:'./assets/coding icons/css3-original.svg',
-        iconText:'./assets/coding icons/css3-original-wordmark.svg',
-        simple: './assets/coding icons/css3-plain.svg',
-        simpleText:'./assets/coding icons/css3-plain-wordmark.svg',
+        icon:cssIcon,
+        iconText:'/src/assets/coding icons/css3-original-wordmark.svg',
+        simple: '/src/assets/coding icons/css3-plain.svg',
+        simpleText:'/src/assets/coding icons/css3-plain-wordmark.svg',
     },
     javascript:{
-        icon:'./assets/coding icons/javascript-original.svg',
+        icon:javascriptIcon,
         iconText:'',
-        simple: './assets/coding icons/javascript-plain.svg',
+        simple: '/src/assets/coding icons/javascript-plain.svg',
         simpleText:'',
     },
     typescript:{
-        icon:'./assets/coding icons/typescript-original.svg',
+        icon:typescriptIcon,
         iconText:'',
-        simple: './assets/coding icons/typescript-plain.svg',
+        simple: '/src/assets/coding icons/typescript-plain.svg',
         simpleText:'',
     },
     npm:{
-        icon:'./assets/coding icons/icons8-npm-480.png',
+        icon:npmIcon,
         iconText:'',
-        simple: './assets/coding icons/codepen-plain.svg',
+        simple: '/src/assets/coding icons/codepen-plain.svg',
         simpleText:'',
     },
 
     figma:{
-        icon:'./assets/coding icons/figma-original.svg',
+        icon:figmaIcon,
         iconText:'',
-        simple: './assets/coding icons/figma-plain.svg',
+        simple: '/src/assets/coding icons/figma-plain.svg',
         simpleText:'',
     },
     github:{
-        icon:'./assets/coding icons/github-original.svg',
-        iconText:'./assets/coding icons/github-original-wordmark.svg',
+        icon:githubIcon,
+        iconText:'/src/assets/coding icons/github-original-wordmark.svg',
         simple: '',
         simpleText:'',
     },
   
 
     react:{
-        icon:'./assets/coding icons/react-original.svg',
-        iconText:'./assets/coding icons/react-original-wordmark.svg',
-        simple: './assets/coding icons/react-original.svg',
-        simpleText:'./assets/coding icons/react-original-wordmark.svg',
+        icon:reactIcon,
+        iconText:'/src/assets/coding icons/react-original-wordmark.svg',
+        simple: '/src/assets/coding icons/react-original.svg',
+        simpleText:'/src/assets/coding icons/react-original-wordmark.svg',
     },
     sass:{
-        icon:'./assets/coding icons/sass-original.svg',
-        iconText:'./assets/coding icons/sass-original.svg',
+        icon:sassIcon,
+        iconText:'/src/assets/coding icons/sass-original.svg',
         simple: '',
         simpleText:'',
     },
     vscode:{
-        icon:'./assets/coding icons/vscode-original.svg',
-        iconText:'./assets/coding icons/vscode-original-wordmark.svg',
-        simple: './assets/coding icons/vscode-plain.svg',
-        simpleText:'./assets/coding icons/vscode-plain-wordmark.svg',
+        icon:vscodeIcon,
+        iconText:'/src/assets/coding icons/vscode-original-wordmark.svg',
+        simple: '/src/assets/coding icons/vscode-plain.svg',
+        simpleText:'/src/assets/coding icons/vscode-plain-wordmark.svg',
     },
 }
 
